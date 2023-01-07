9cfb4e0bd3e00ba0972420db9e58eb03c0b943e5
me105 <me105@tadbox.com>
2023-01-07 23:26:04 +0300

    -

---
+++ src/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-header bordered class="header bg-gray-500">
+    <div class="header__container"></div>
+  </q-header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="bg-background">
+    <v-header />
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+      <q-list>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  components: {
+    "v-header": async () => await import("components/header.vue")
+  },
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  }
+};
+</script>
