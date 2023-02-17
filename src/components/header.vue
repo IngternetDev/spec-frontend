1a8709165bd7ad0a0665f0ec96a690c3fcb180c4
Ramzi523Scl <101909604+Ramzi523Scl@users.noreply.github.com>
2023-02-17 14:52:59 +0300

    added the initial layout

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,17 @@
 <template>
   <q-header bordered class="header bg-gray-500">
-    <div class="header__container">
-      qwe
+    <div class="header__content">
+      <img class="header__logo" src="../assets/header/logo.svg" />
+      <div class="header__profile">
+        <img src="../assets/header/ava.png" />
+      </div>
+      <img class="header__theme" src="../assets/header/sun.svg" />
     </div>
   </q-header>
 </template>
 
 <script>
-
-export default {
-};
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
